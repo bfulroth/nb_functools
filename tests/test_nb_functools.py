a2bf609bc82bc2e82ef03a5c3de441cb4789fe13
Ben Fulroth <fulrothb@icloud.com>
2024-08-01 12:17:02 -0400

    Added my first unit test

--- tests/test_nb_functools.py
+++ tests/test_nb_functools.py
@@ -1,6 +1,8 @@
 
-from nb_functools import main
+from nb_functools import nb_functools
 
 
-def test_main():
-    pass
+def test_calc_m1v1_m1v2():
+
+    result = nb_functools.calc_m1v1_m2v2(m1=10000, v1=None, m2=50, v2=200)
+    assert result == 2
